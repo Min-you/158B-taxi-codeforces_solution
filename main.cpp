e35a5fffbf7ce826801e2fe394e5b6b989cb386b
Min-you <101444459+Min-you@users.noreply.github.com>
2022-09-10 09:20:06 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,27 +1,41 @@
 #include <bits/stdc++.h>
+#define in cin>>
+#define out cout<<
+#define M 100002
+#define FOR(i,k,l)   for(int i(k); i<l; i++)
+#define Si set<int>
+#define Mii map<int,int>
+#define Mss map<string,string>
+#define Vi vector<int>
+#define Di deque<int>
+
 using namespace std;
-typedef long long ll;
-typedef vector <int> vi;
 
 int main(){
-    ll n;
-    // get the number of groups
-    cin >> n;
-    int c = 0;
-    int s = 0;
-    // get the number of students
-    for (int i = 0; i < n; i++){
-        int temp;
-        cin >> temp;
-        s += temp;
+ios_base::sync_with_stdio(false);
+int n,taxi=0;
+    in(n);
+    Vi v(M);
+
+    FOR(i,0,n){
+        in(v[i]);
     }
-    //divide the students according the amount a car can hold 
-    if (s%4 == 0){
-        s = s/4;
-    }
-    else {
-        s = s/4;
-        s++;
-    }
-    cout << s << endl;
+    sort(v.begin(),v.end());
+
+        int i=v.size()-1;
+        int k=0;
+        while(k!= i){
+            if(v[i]+ v[k]<=4){
+                v[i]+=v[k];
+                k++;
+            }
+            else{
+                i--;
+                taxi++;
+            }
+        }
+    out(taxi+1);
+
+
+return 0;
 }
