3dffda332ca1904ddc5b0c4b64afc5a59639018a
Min-you <101444459+Min-you@users.noreply.github.com>
2022-09-10 09:04:46 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,14 +5,17 @@ typedef vector <int> vi;
 
 int main(){
     ll n;
+    // get the number of groups
     cin >> n;
     int c = 0;
     int s = 0;
+    // get the number of students
     for (int i = 0; i < n; i++){
         int temp;
         cin >> temp;
         s += temp;
     }
+    //divide the students according the amount a car can hold 
     if (s%4 == 0){
         s = s/4;
     }
